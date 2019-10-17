ff85fcbf138ed1a2af84a98f9f6d99c728db1ded
ampersand <hossainazifa@gmail.com>
2019-10-17 11:09:31 -0700

    MVP

---
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerIntegrationTest.java
@@ -0,0 +1,56 @@
+package com.lambdaschool.school.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Instructor;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CourseControllerIntegrationTest
+{
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void initialiseRestAssuredMockMvcWebApplicationContext()
+    {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
+   //GET: /courses/courses
+    @Test
+    public void whenMeasuredReponseTime()
+    {
+        given().when().get("/courses/courses").then().time(lessThan(5000L));
+    }
+
+
+    //    POST /courses/course/add
+    @Test
+    public void givenPostACourse() throws Exception
+    {
+        String c9name = "Course 9";
+        Instructor instr9 = new Instructor("Dr.Instr9");
+        Course c9 = new Course(c9name, instr9);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String stringC9 = mapper.writeValueAsString(c9);
+
+        given().contentType("application/json").body(stringC9).when().post("/courses/course/add").then().statusCode(201);
+    }
+
+}
